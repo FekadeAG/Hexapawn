776f364590aeb73ae5d0d7237c29001e0e76d8b5
s220196982agf <s220196982@mandela.ac.za>
2022-10-27 22:37:11 +0200

    Add project files.

---
+++ Hexapawn/Assets/Board.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hexapawn.Assets
+{
+    public class Board
+    {
+        public int Size { get; set; }
+        public Board()
+        {
+            Size = 3;
+        }
+    }
+}
